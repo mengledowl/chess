4d7cbff3ad85ad2171eddd83c5b4b19b1a7ae7c4
Matt Engledowl <mengledowl@gmail.com>
2017-12-30 01:46:28 -0600

    Require piece and position arguments

--- lib/chess.rb
+++ lib/chess.rb
@@ -2,10 +2,13 @@
 
 require_relative 'chess/chess_moves_finder'
 
-piece_index = ARGV.index('-piece') + 1
-position_index = ARGV.index('-position') + 1
+piece_index = ARGV.index('-piece')
+position_index = ARGV.index('-position')
 
-piece = ARGV[piece_index]
-position = ARGV[position_index]
+abort 'Error: -piece argument is required' unless piece_index
+abort 'Error: -position argument is required' unless position_index
+
+piece = ARGV[piece_index + 1]
+position = ARGV[position_index + 1]
 
 puts ChessMovesFinder.new(piece, at: position).retrieve_available_moves.join(', ')
